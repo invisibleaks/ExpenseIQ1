684ac83b34a4192c326e15134e1e9e90f004f678
invisibleaks <abhishek.singh22@gmail.com>
2025-08-02 13:31:50 +0530

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class', // ✅ ADD THIS LINE
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
